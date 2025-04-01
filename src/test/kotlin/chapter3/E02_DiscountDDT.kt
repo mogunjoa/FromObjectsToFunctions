b6c4bf16e3e70d8671fab4582eb248fa9e3f0d53
seungohjin <corrobolea@gmail.com>
2025-04-01 18:14:57 +0900

    refactor: 3장 연습문제 1, 2번 파일 분리

--- src/test/kotlin/chapter3/E02_DiscountDDT.kt
+++ src/test/kotlin/chapter3/E02_DiscountDDT.kt
@@ -5,19 +5,19 @@ import com.ubertob.pesticide.core.DomainDrivenTest
 import org.mogun.chapter3.Item.carrot
 import org.mogun.chapter3.Item.milk
 
-
-class E01_CashierDDT: DomainDrivenTest<CashierActions>(allActions) {
+class E02_DiscountDDT: DomainDrivenTest<CashierActions>(allActions) {
     val alice by NamedActor(::CustomerActor)
 
     @DDT
-    fun `customer can buy an item`() = ddtScenario {
+    fun `customer can benefit from 3x2 offer`() = ddtScenario {
         val prices = mapOf(carrot to 2.0, milk to 5.0)
         setUp {
             setupPrices(prices)
+            setup3x2(milk)
         }.thenPlay(
             alice.`can add #qty #item`(3, carrot),
-            alice.`can add #qty #item`(1, milk),
-            alice.`check total is #total`(11.0)
+            alice.`can add #qty #item`(3, milk),
+            alice.`check total is #total`(16.0)
         )
     }
 }
