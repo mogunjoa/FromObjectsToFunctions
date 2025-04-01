0da26b44190e536c3f8e4bb9548b1c0b7b191505
seungohjin <corrobolea@gmail.com>
2025-04-01 17:34:40 +0900

    feat: pesticide 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("com.ubertob.pesticide:pesticide-core:1.6.6")
     testImplementation("io.strikt:strikt-core:0.34.0")
 }
 
